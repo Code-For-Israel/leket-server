082a487a9bdcba30eda614ef40bf9d9d1deeee74
Guy-Bilitski <guyb181@gmail.com>
2023-06-12 12:34:56 +0300

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,13 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(() => console.log('Server started'));
